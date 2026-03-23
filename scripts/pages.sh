697e26df6ae9453a452b8c5078748910f5461d73
feelbadinc <132283466+feelbadinc@users.noreply.github.com>
2026-03-24 01:23:51 +0300

    fix readme

--- scripts/pages.sh
+++ scripts/pages.sh
@@ -5,18 +5,19 @@ git fetch origin pages 2>/dev/null \
   && git worktree add --no-checkout _pages pages \
   || git worktree add --orphan -b pages _pages
 
-mv _source/build/web/* _pages/
+DATE=$(date -u +%Y-%m-%d)
 
+mv _source/build/web/* _pages/
 cp LICENSE _pages/
 
 cat > _pages/README.md << EOF
-> Built from https://github.com/$UPSTREAM_REPO
+> Built from https://github.com/$UPSTREAM_REPO<br>
 > by https://github.com/$GITHUB_REPOSITORY/tree/master
 >
-> Last build: $(date -u +%Y-%m-%d)
+> Last build: $DATE
 EOF
 
 cd _pages
 git add -A
-git commit -m "build $(date -u +%Y%m%d)"
+git commit -m "build $DATE"
 git push origin pages
