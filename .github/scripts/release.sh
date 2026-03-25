a6caea924ccd8c49486769420d8715d661526669
feelbadinc <132283466+feelbadinc@users.noreply.github.com>
2026-03-25 17:56:28 +0300

    reformat changelogs

--- .github/scripts/release.sh
+++ .github/scripts/release.sh
@@ -5,9 +5,9 @@ mv _source/LICENSE _source/build/web/
 (cd _source/build/web && zip -r "$GITHUB_WORKSPACE/release.zip" .)
 
 TAG="lampa-$(date +'%Y-%m-%d')"
-CHANGELOG=$(git -C _source --no-pager log --pretty=format:"- %h | %s")
+CHANGELOG=$(git -C _source --no-pager log --pretty=format:'- `%h` %s')
 if [ -n "$CHANGELOG" ]; then
-  NOTES="$(printf "Upstream changes:\n\n%s" "$CHANGELOG")"
+  NOTES="$(printf "**Upstream changes**:\n\n%s" "$CHANGELOG")"
 else
   NOTES="No upstream changes."
 fi
